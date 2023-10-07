72290bb8553ec9af85c903c8832a29decb96603c
azfang <47017488+azfang@users.noreply.github.com>
2023-10-07 13:24:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 $(document).ready(function(){
 
     /*! Fades in page on load */
-    $('body').hide().delay(300).fadeIn(500)
+    // $('.button').css('display', 'none');
+    $('body').hide().delay(300).fadeIn(500);
+    // $('#textline1').hide();
+    $('.textline1').delay(8000).fadeOut(2000);
+    // $('.textline1').hide().delay(3000).fadeIn(1000);
+    // $('.button').hide().delay(300).fadeIn(500);
     
     });
 
-// var ppbutton = document.getElementById("vidbutton");
-// ppbutton.addEventListener("click", playPause);
-// vid1 = document.getElementById("vid1");
-// function playPause() { 
-//     if (vid1.paused) {
-//         vid1.play();
-//         vid1.loop = true;
-//         ppbutton.innerHTML = "{  ting shi  }";
-//         }
-//     else  {
-//         vid1.pause(); 
-//         ppbutton.innerHTML = "{  kai shi  }";
-//         }
-// } 
+var ppbutton = document.getElementById("playVideo");
+ppbutton.addEventListener("click", playVideo);
+function playVideo() { 
+    $('.vid').trigger('play');
+} 
+
+var ppbutton = document.getElementById("pauseVideo");
+ppbutton.addEventListener("click", pauseVideo);
+function pauseVideo() { 
+    $('.vid').trigger('pause');
+} 
 
 // var ppbutton2 = document.getElementById("vid2button");
 // ppbutton2.addEventListener("click", playPause2);
@@ -32,215 +34,5 @@ $(document).ready(function(){
 //     else  {
 //         vid2.pause(); 
 //         ppbutton2.innerHTML = "{  play  }";
-//         }
-// } 
-
-// var ppbutton3 = document.getElementById("vid3button");
-// ppbutton3.addEventListener("click", playPause3);
-// vid3 = document.getElementById("vid3");
-// function playPause3() { 
-//     if (vid3.paused) {
-//         vid3.play();
-//         vid3.loop = true;
-//         ppbutton3.innerHTML = "{  pause  }";
-//         }
-//     else  {
-//         vid3.pause(); 
-//         ppbutton3.innerHTML = "{  play  }";
-//         }
-// } 
-
-// var ppbutton4 = document.getElementById("vid4button");
-// ppbutton4.addEventListener("click", playPause4);
-// vid4 = document.getElementById("vid4");
-// function playPause4() { 
-//     if (vid4.paused) {
-//         vid4.play();
-//         vid4.loop = true;
-//         ppbutton4.innerHTML = "{  pause  }";
-//         }
-//     else  {
-//         vid4.pause(); 
-//         ppbutton4.innerHTML = "{  play  }";
-//         }
-// } 
-
-// var ppbutton5 = document.getElementById("vid5button");
-// ppbutton5.addEventListener("click", playPause5);
-// vid5 = document.getElementById("vid5");
-// function playPause5() { 
-//     if (vid5.paused) {
-//         vid5.play();
-//         vid5.loop = true;
-//         ppbutton5.innerHTML = "{  pause  }";
-//         }
-//     else  {
-//         vid5.pause(); 
-//         ppbutton5.innerHTML = "{  play  }";
-//         }
-// } 
-
-// var ppbutton6 = document.getElementById("vid6button");
-// ppbutton6.addEventListener("click", playPause6);
-// vid6 = document.getElementById("vid6");
-// function playPause6() { 
-//     if (vid6.paused) {
-//         vid6.play();
-//         vid6.loop = true;
-//         ppbutton6.innerHTML = "{  pause  }";
-//         }
-//     else  {
-//         vid6.pause(); 
-//         ppbutton6.innerHTML = "{  play  }";
-//         }
-// } 
-
-// var ppbutton7 = document.getElementById("vid7button");
-// ppbutton7.addEventListener("click", playPause7);
-// vid7 = document.getElementById("vid7");
-// function playPause7() { 
-//     if (vid7.paused) {
-//         vid7.play();
-//         vid7.loop = true;
-//         ppbutton7.innerHTML = "{  pause  }";
-//         }
-//     else  {
-//         vid7.pause(); 
-//         ppbutton7.innerHTML = "{  play  }";
-//         }
-// } 
-
-// var ppbutton8 = document.getElementById("vid8button");
-// ppbutton8.addEventListener("click", playPause8);
-// vid8 = document.getElementById("vid8");
-// function playPause8() { 
-//     if (vid8.paused) {
-//         vid8.play();
-//         vid8.loop = true;
-//         ppbutton8.innerHTML = "{  pause  }";
-//         }
-//     else  {
-//         vid8.pause(); 
-//         ppbutton8.innerHTML = "{  play  }";
-//         }
-// } 
-
-// var ppbutton9 = document.getElementById("vid9button");
-// ppbutton9.addEventListener("click", playPause9);
-// vid9 = document.getElementById("vid9");
-// function playPause9() { 
-//     if (vid9.paused) {
-//         vid9.play();
-//         vid9.loop = true;
-//         ppbutton9.innerHTML = "{  pause  }";
-//         }
-//     else  {
-//         vid9.pause(); 
-//         ppbutton9.innerHTML = "{  play  }";
-//         }
-// } 
-
-// var ppbutton10 = document.getElementById("vid10button");
-// ppbutton10.addEventListener("click", playPause10);
-// vid10 = document.getElementById("vid10");
-// function playPause10() { 
-//     if (vid10.paused) {
-//         vid10.play();
-//         vid10.loop = true;
-//         ppbutton10.innerHTML = "{  pause  }";
-//         }
-//     else  {
-//         vid10.pause(); 
-//         ppbutton10.innerHTML = "{  play  }";
-//         }
-// } 
-
-// var ppbutton11 = document.getElementById("vid11button");
-// ppbutton11.addEventListener("click", playPause11);
-// vid11 = document.getElementById("vid11");
-// function playPause11() { 
-//     if (vid11.paused) {
-//         vid11.play();
-//         vid11.loop = true;
-//         ppbutton11.innerHTML = "{  pause  }";
-//         }
-//     else  {
-//         vid11.pause(); 
-//         ppbutton11.innerHTML = "{  play  }";
-//         }
-// } 
-
-// var ppbutton12 = document.getElementById("vid12button");
-// ppbutton12.addEventListener("click", playPause12);
-// vid12 = document.getElementById("vid12");
-// function playPause12() { 
-//     if (vid12.paused) {
-//         vid12.play();
-//         vid12.loop = true;
-//         ppbutton12.innerHTML = "{  pause  }";
-//         }
-//     else  {
-//         vid12.pause(); 
-//         ppbutton12.innerHTML = "{  play  }";
-//         }
-// } 
-
-// var ppbutton13 = document.getElementById("vid13button");
-// ppbutton13.addEventListener("click", playPause13);
-// vid13 = document.getElementById("vid13");
-// function playPause13() { 
-//     if (vid13.paused) {
-//         vid13.play();
-//         vid13.loop = true;
-//         ppbutton13.innerHTML = "{  pause  }";
-//         }
-//     else  {
-//         vid13.pause(); 
-//         ppbutton13.innerHTML = "{  play  }";
-//         }
-// } 
-
-// var ppbutton14 = document.getElementById("vid14button");
-// ppbutton14.addEventListener("click", playPause14);
-// vid14 = document.getElementById("vid14");
-// function playPause14() { 
-//     if (vid14.paused) {
-//         vid14.play();
-//         vid14.loop = true;
-//         ppbutton14.innerHTML = "{  pause  }";
-//         }
-//     else  {
-//         vid14.pause(); 
-//         ppbutton14.innerHTML = "{  play  }";
-//         }
-// } 
-
-// var ppbutton15 = document.getElementById("vid15button");
-// ppbutton15.addEventListener("click", playPause15);
-// vid15 = document.getElementById("vid15");
-// function playPause15() { 
-//     if (vid15.paused) {
-//         vid15.play();
-//         vid15.loop = true;
-//         ppbutton15.innerHTML = "{  pause  }";
-//         }
-//     else  {
-//         vid15.pause(); 
-//         ppbutton15.innerHTML = "{  play  }";
-//         }
-// } 
-
-// var ppbutton16 = document.getElementById("vid16button");
-// ppbutton16.addEventListener("click", playPause16);
-// vid16 = document.getElementById("vid16");
-// function playPause16() { 
-//     if (vid16.paused) {
-//         vid16.play();
-//         vid16.loop = true;
-//         ppbutton16.innerHTML = "{  pause  }";
-//         }
-//     else  {
-//         vid16.pause(); 
-//         ppbutton16.innerHTML = "{  play  }";
 //         }
 // } 
